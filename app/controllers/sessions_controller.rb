484252c1d592ce1d83048124f606ac41c0c1ed6a
Takuya Okuhara <okutaku0507@gmail.com>
2013-05-24 20:04:24 +0900

    sessionのバグを解消

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,15 @@
 
 class SessionsController < ApplicationController
 
- def create
+  def create
     member = Member.authenticate(params[:email], params[:password])
     if member
       cookies.signed[:member_id] = { value: member.id, expires: 30.days.from_now }
+      redirect_to params[:from] || :account
     else
       flash.alert = "※メールアドレスとパスワードが一致しません"
+          redirect_to params[:from] || :course_infos
     end
-    redirect_to params[:from] || :account
   end
 
   def destroy
