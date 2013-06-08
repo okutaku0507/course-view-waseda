14594c0507592e9be0b03cce59182263ec9e454e
Takuya Okuhara <okutaku0507@gmail.com>
2013-06-08 16:16:37 +0900

    cap_2

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,11 +5,16 @@ set :rails_env, "production"
 
 server "course-view.com", :web, :app, :db, primary: true
 
-set :repository, "ssh://git@bitbucket.org:suzukiokuhara/course-view-waseda.git"
+set :repository, "git@bitbucket.org:suzukiokuhara/course-view-waseda.git"
 set :scm, :git
 set :branch, "master"
 set :user, "rails"
 set :use_sudo, false
-set :deploy_to, "/var/#{user}/course-view-waseda"
-set :deploy_via, :remote_cache
+set :deploy_to, "/var/#{user}/#{rails_env}"
+set :deploy_via, :copy
+set :copy_cache, true
 ssh_options[:forward_agent] = true
+
+after "deploy:update", roles: :app do
+  run "bin/cp #{shared_path}/config/database.yml #{release_path}/config/"
+end
