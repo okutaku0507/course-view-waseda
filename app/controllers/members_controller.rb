8571327bbba09ea5d8463abeeb2dc8689458cf91
Takuya Okuhara <okutaku0507@gmail.com>
2015-07-09 21:01:00 +0900

    微調整

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class MembersController < ApplicationController
-  
+
   def new
     @member = Member.new
   end
@@ -53,7 +53,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "法学部"
       unless @member.subject == "無し"
         flash.alert = "法学部の方は'無し'を選択してください。"
@@ -73,7 +73,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "第一文学部"
       unless @member.subject == "無し"
         flash.alert = "第一文学部の方は'無し'を選択してください。"
@@ -83,7 +83,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "第二文学部"
       unless @member.subject == "無し"
         flash.alert = "第二文学部の方は'無し'を選択してください。"
@@ -93,7 +93,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "文化構想学部"
       unless @member.subject == "無し"
         flash.alert = "文化構想学部の方は'無し'を選択してください。"
@@ -103,7 +103,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "文学部"
       unless @member.subject == "無し"
         flash.alert = "文学部の方は'無し'を選択してください。"
@@ -113,7 +113,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "教育学部"
       unless @member.subject == "教育学専修" || @member.subject == "生涯教育学専修" || @member.subject == "教育心理学専修" || @member.subject == "初等教育学専攻" || @member.subject == "国語国文学科" || @member.subject == "英語英文学科" || @member.subject == "地理歴史専修" ||  @member.subject == "社会科学専修" || @member.subject == "生物学専修" || @member.subject == "地球科学専修" || @member.subject == "数学科" || @member.subject == "複合文化学科"
         flash.alert =  "学部と学科が一致しません。"
@@ -123,7 +123,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "商学部"
       unless @member.subject == "無し"
         flash.alert = "商学部の方は'無し'を選択してください。"
@@ -133,7 +133,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "基幹理工学部"
       unless @member.subject == "無し" || @member.subject == "数学科" || @member.subject == "応用数理学科" || @member.subject == "情報理工学科" || @member.subject == "機械科学・航空学科" || @member.subject == "電子光システム学科" || @member.subject == "表現工学科"
         flash.alert = "学部と学科が一致しません。また、一年生は'無し'を選択してください。"
@@ -143,7 +143,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "創造理工学部"
       unless @member.subject == "建築学科" || @member.subject == "総合機械工学科" || @member.subject == "経営システム工学科" || @member.subject == "社会環境工学科" || @member.subject == "環境資源工学科"
         flash.alert = "学部と学科が一致しません。"
@@ -153,7 +153,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "先進理工学部"
       unless @member.subject == "物理学科" || @member.subject == "応用物理学科" || @member.subject == "化学・生命化学" || @member.subject == "応用化学科" || @member.subject == "生命医科学科" || @member.subject == "電気・情報生命工学科"
         flash.alert = "学部と学科が一致しません。"
@@ -173,7 +173,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "スポーツ科学部"
       unless @member.subject == "無し"
         flash.alert = "スポーツ科学部の方は'無し'を選択してください。"
@@ -183,7 +183,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
      if  @member.faculty == "国際教養学部"
       unless @member.subject == "無し"
         flash.alert = "国際教養学部の方は'無し'を選択してください。"
@@ -193,7 +193,7 @@ class MembersController < ApplicationController
         return true
       end
     end
-    
+
     if  @member.faculty == "その他"
       unless @member.subject == "その他"
         flash.alert = "その他を選んだ方は'その他'を選択してください。"
@@ -204,5 +204,5 @@ class MembersController < ApplicationController
       end
     end
   end
-  
+
 end
